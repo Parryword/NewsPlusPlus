dcb68f94b9b24c8fa7ec0deb2a04652152ddef9e
Parryword <yigit.efe@std.ieu.edu.tr>
2024-03-18 01:02:45 +0300

    popup menu

--- lib/news.dart
+++ lib/news.dart
@@ -1,48 +1,89 @@
 import 'package:flutter/material.dart';
 import 'network.dart';
 
-class NewsWidget extends StatelessWidget {
+class NewsWidget extends StatefulWidget {
   final String title;
   final String description;
   final String url;
 
-  const NewsWidget({super.key,
+  const NewsWidget({
+    super.key,
     required this.title,
     required this.description,
     required this.url,
   });
 
   @override
+  State<StatefulWidget> createState() => _NewsState();
+}
+
+/// "Proto Coders Point" https://www.youtube.com/watch?v=XHroTEqZJb8
+class _NewsState extends State<NewsWidget> {
+  var _tapPosition = Offset.zero;
+  void _getTabPosition(TapDownDetails details) {
+    setState(() {
+      _tapPosition = details.globalPosition;
+      debugPrint(_tapPosition.toString());
+    });
+  }
+
+  Future<void> _showContextMenu(context) async {
+    final RenderObject? overlay = Overlay.of(context)?.context.findRenderObject();
+    final result = await showMenu(
+        context: context,
+        position: RelativeRect.fromRect(
+          Rect.fromLTWH(_tapPosition.dx, _tapPosition.dy, 10, 10),
+          Rect.fromLTWH(0, 0, overlay!.paintBounds.size.width, overlay!.paintBounds.size.height)
+        ),
+        items: [
+          const PopupMenuItem(child: Text("Bookmark"),),
+          const PopupMenuItem(child: Text("Copy link"),),
+          const PopupMenuItem(child: Text("Open"),),
+        ]);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Container(
-        padding: const EdgeInsets.all(8.0),
-        color: Colors.indigo.shade50,
-        child: InkWell(
-          onTap: () {
-            BrowserView(url: url).launch();
-            },
-          onLongPress: () {
-            // TODO: Implement menu to save news, add bookmark, send mail to friend feature here
-          },
-          child: Column(
-            children: [
-              Align(
-                alignment: Alignment.topLeft,
-                child: Text(title, style: const TextStyle(fontWeight: FontWeight.bold),),
-              ),
-              Align(
-                  alignment: Alignment.topLeft,
-                  child: Text(description.replaceAll("\n", ""))),
-              Align(
-                  alignment: Alignment.topLeft,
-                  child: Text(url)),
-            ],
-          ),
+      child: GestureDetector(
+        onTapDown: (position) {
+          _getTabPosition(position);
+        },
+        onLongPress: (){
+          _showContextMenu(context);
+        },
+        child: Container(
+            padding: const EdgeInsets.all(8.0),
+            color: Colors.indigo.shade50,
+            child: Stack(
+              children: <Widget>[
+                InkWell(
+                  onTap: () {
+                    BrowserView(url: widget.url).launch();
+                  },
+                  child: Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          widget.title,
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Align(
+                          alignment: Alignment.topLeft,
+                          child: Text(widget.description.replaceAll("\n", ""))),
+                      Align(alignment: Alignment.topLeft, child: Text(widget.url)),
+                    ],
+                  ),
+                ),
+              ],
+            )
         ),
       ),
     );
+
   }
 }
 
@@ -63,14 +104,15 @@ class News {
   factory News.fromJson(Map<String, dynamic> json) {
     return switch (json) {
       {
-      'title': String title,
-      'description': String description,
-      'url': String url,
-      } => News (
-        title: title,
-        description: description,
-        url: url,
-      ),
+        'title': String title,
+        'description': String description,
+        'url': String url,
+      } =>
+        News(
+          title: title,
+          description: description,
+          url: url,
+        ),
       _ => throw const FormatException("Failed to load news."),
     };
   }
